6ab209c55e04381bcb8cd685131b30b14c2d4bba
anboch <a.g.bochka@gmail.com>
2023-02-07 21:11:45 +0600

    feat: add auto creation of db

--- src/database/typeorm.service.ts
+++ src/database/typeorm.service.ts
@@ -1,10 +1,11 @@
 import 'reflect-metadata';
 import { inject, injectable } from 'inversify';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { createDatabase } from 'typeorm-extension';
 
 import { ILogger } from '../logger/logger.service';
 import { TYPES } from '../common/constants';
 import { IConfigService } from '../config/config.service';
-import { DataSource } from 'typeorm';
 import { User } from '../user/user.entity';
 import { JWTBlacklist, Session } from '../auth/auth.entity';
 import { FileInfo } from '../file/file.entity';
@@ -12,12 +13,13 @@ import { FileInfo } from '../file/file.entity';
 @injectable()
 export class TypeormService {
 	dataSource: DataSource;
+	dataSourceOptions: DataSourceOptions;
 
 	constructor(
 		@inject(TYPES.ConfigService) private configService: IConfigService,
 		@inject(TYPES.ILogger) private logger: ILogger,
 	) {
-		this.dataSource = new DataSource({
+		this.dataSourceOptions = {
 			type: 'mysql',
 			host: this.configService.get('MY_SQL_DB_HOST'),
 			port: Number(this.configService.get('MY_SQL_DB_PORT')),
@@ -27,11 +29,13 @@ export class TypeormService {
 			entities: [User, JWTBlacklist, Session, FileInfo],
 			synchronize: true,
 			logging: false,
-		});
+		};
+		this.dataSource = new DataSource(this.dataSourceOptions);
 	}
 
 	init = async (): Promise<void> => {
 		try {
+			await createDatabase({ options: this.dataSourceOptions });
 			await this.dataSource.initialize();
 
 			this.logger.log('[TypeormService] Successful connection to the database');
